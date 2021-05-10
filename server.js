c3c60746f36797c100e2bb07dbe818b0016a6eea
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-10 23:56:48 +0930

    fixing

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 
 const routes = require("./routes");
 const app = express();
@@ -10,12 +10,13 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.use(routes);
 
-
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
