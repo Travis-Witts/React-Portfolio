595845de5ac07de698ab762162fa8a0f17709057
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-06-11 02:49:58 +0930

    fix: fixing resume bug

--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -24,22 +24,58 @@ function Projects() {
 
         <div className="row project-container">
           <div className=" py-3 col-lg-4 col-md-6 ">
-            <Project deploy="https://travis-witts.github.io/Searchify/" github="https://github.com/Travis-Witts/Searchify" name="Searchify" desc="Find live music near you!" img={Searchify}/>
+            <Project
+              deploy="https://travis-witts.github.io/Searchify/"
+              github="https://github.com/Travis-Witts/Searchify"
+              name="Searchify"
+              desc="Find live music near you!"
+              img={Searchify}
+            />
           </div>
           <div className="py-3 col-lg-4 col-md-6 ">
-            <Project deploy="https://anypos.herokuapp.com/" github="https://github.com/Travis-Witts/AnyPOS" name="AnyPOS" desc="Sell your products on the go!" img={Anypos}/>
+            <Project
+              deploy="https://anypos.herokuapp.com/"
+              github="https://github.com/Travis-Witts/AnyPOS"
+              name="AnyPOS"
+              desc="Sell your products on the go!"
+              img={Anypos}
+            />
           </div>
           <div className="py-3 col-lg-4 col-md-6 ">
-            <Project deploy="https://handlebars-tech-blog.herokuapp.com/" github="https://github.com/Travis-Witts/tech-blog" name="Tech Blog" desc="Share what's on your mind!" img={Blog}/>
+            <Project
+              deploy="https://handlebars-tech-blog.herokuapp.com/"
+              github="https://github.com/Travis-Witts/tech-blog"
+              name="Tech Blog"
+              desc="Share what's on your mind!"
+              img={Blog}
+            />
           </div>
           <div className="py-3 col-lg-4 col-md-6 ">
-            <Project deploy="https://travis-witts.github.io/User-Directory/" github="https://github.com/Travis-Witts/User-Directory" name="User Directory" desc="A simple react app to sort users or employees." img={Users}/>
+            <Project
+              deploy="https://travis-witts.github.io/User-Directory/"
+              github="https://github.com/Travis-Witts/User-Directory"
+              name="User Directory"
+              desc="A simple react app to sort users or employees."
+              img={Users}
+            />
           </div>
           <div className="py-3 col-lg-4 col-md-6 ">
-            <Project deploy="https://travis-witts.github.io/weather-dashboard/" github="https://github.com/Travis-Witts/weather-dashboard" name="Weather DashBoard" desc="Find out the forecast in your city!" img={Weather}/>
+            <Project
+              deploy="https://travis-witts.github.io/weather-dashboard/"
+              github="https://github.com/Travis-Witts/weather-dashboard"
+              name="Weather DashBoard"
+              desc="Find out the forecast in your city!"
+              img={Weather}
+            />
           </div>
           <div className="py-3 col-lg-4 col-md-6 ">
-            <Project deploy="https://travis-witts.github.io/code-quiz/" github="https://github.com/Travis-Witts/code-quiz" name="Code Quiz" desc="A fun little Jquery and JS code quiz." img={Quiz}/>
+            <Project
+              deploy="https://travis-witts.github.io/code-quiz/"
+              github="https://github.com/Travis-Witts/code-quiz"
+              name="Code Quiz"
+              desc="A fun little Jquery and JS code quiz."
+              img={Quiz}
+            />
           </div>
         </div>
       </div>
